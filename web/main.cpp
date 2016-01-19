56c61666f724ddde7cf59fd5f401446a488f0e2d
owen <stensonowen@gmail.com>
2016-01-19 08:47:09 -0500

    Added cache to homepage

--- web/main.cpp
+++ web/main.cpp
@@ -1,15 +1,17 @@
-//this file:
-#include<iostream>
+//g++ main.cpp -std=c++11 -lboost_system -pthread -lsqlite3
+//#include<iostream>
 //crow
 #include "../include/crow.h"
 #include "../include/mustache.h"
 //#include "../crow/include/json.h"
 //table headers
+#include<iostream>
 #include "../src/table.h"
-
-//g++ main.cpp -std=c++11 -lboost_system -pthread
+#include "cache.h"
 
 using namespace std;
+void populate_ctx(crow::mustache::context&, Table&, Cache&, std::string, std::string);
+std::string format_link(const Abs_path &ap);
 
 int main(int argc, char* argv[]){
     //construct table:
@@ -17,7 +19,9 @@ int main(int argc, char* argv[]){
         std::cerr << "usage: WikiLinks.out path_to_parsed_file" << std::endl;
         exit(1);
     }
+    //construct data objects
     Table t(argv[1]);
+    Cache cache("cache1.db");
 
     //construct web fw
     crow::SimpleApp app;
@@ -25,38 +29,61 @@ int main(int argc, char* argv[]){
 
     //define locations a la flask
     CROW_ROUTE(app, "/")
-        ([]() {
-         //TODO: create index.html
-         return "Hell World\n";
-         });
-   
+        ([&t, &cache](const crow::request& req){
+        std::string src, dst;
+        crow::mustache::context ctx;
+        src = (req.url_params.get("src") == nullptr ? "" : req.url_params.get("src"));
+        dst = (req.url_params.get("dst") == nullptr ? "" : req.url_params.get("dst"));
+        populate_ctx(ctx, t, cache, src, dst);
+        return crow::mustache::load("bfs.html").render(ctx);
+        });
+
+    //search via url 
     CROW_ROUTE(app, "/bfs/<string>/<string>")
-        ([&t](const crow::request& req, crow::response& res, string src, string dst){
+        ([&t, &cache](const crow::request& req, crow::response& res, string src, string dst){
          crow::mustache::context ctx;
-         ctx["src"] = src;
-         ctx["dst"] = dst;
-         ctx["path"] = t.htmlPath(src, dst);
+         populate_ctx(ctx, t, cache, src, dst);
          res.write(crow::mustache::load("bfs.html").render(ctx));
          res.end();
          });
 
-    CROW_ROUTE(app, "/bfs")
-        ([&t](const crow::request& req){
-         std::string src, dst;
-         crow::mustache::context ctx;
-         src = (req.url_params.get("src") == nullptr ? "" : req.url_params.get("src"));
-         dst = (req.url_params.get("dst") == nullptr ? "" : req.url_params.get("dst"));
-         ctx["src"] = src;
-         ctx["dst"] = dst;
-         ctx["path"] = t.htmlPath(src, dst);
-         return crow::mustache::load("bfs.html").render(ctx);
-         });
-
-    app.port(80).multithreaded().run();
+    app.port(8080).multithreaded().run();
 
     
     return 0;
 }
 
+void populate_ctx(crow::mustache::context &ctx, Table &t, Cache &cache, 
+        std::string src, std::string dst){
+    ctx["src"] = src;
+    ctx["dst"] = dst;
+    unsigned int src_, dst_;
+    std::string result = t.contains(src, dst, src_, dst_);
+    if(!result.empty()) ctx["path"] = result;
+    else {
+        //valid input
+        Abs_path *ap = cache.contains(src, dst);
+        if(ap) ctx["path"] = t.htmlPath(ap->path);
+        else {
+            //need to generate path
+            Path path = t.search(src_, dst_);
+            cache.insert(src, dst, path, -1);
+            ctx["path"] = t.htmlPath(path);
+        }
+    }
+    vector<Abs_path> aps = cache.retrieve(20, Cache::sort_by::popular);
+    std::string history;
+    for(unsigned int i=0; i<aps.size(); i++){
+        history += format_link(aps[i]);
+    }
+    ctx["cache"] = history;
+}
 
-
+std::string format_link(const Abs_path &ap){
+    //format to be selectable option
+    std::string result = "<form><input name=\"src\" value=\"" + ap.src + "\" readonly />";
+    result += "&rarr; " + to_string(ap.path.size()) + " &rarr; ";   //subtract? 2?
+    result += "<input name=\"dst\" value=\"" + ap.dst + "\" readonly /> &nbsp;";
+    result += "<input type=\"submit\" name=\"submit\" value=\"Submit\"/></form>\n";
+    return result;
+}
