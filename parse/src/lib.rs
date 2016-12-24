a6b92bc9f6d649dc2f28492baf5cacb0c88b283b
owen <stensonowen@gmail.com>
2016-12-24 16:36:52 -0500

    Start nom parser (should be faster than regexes)
    Still problematic: escaped! is weird, and doesn't match strings that end
    in escaped quotes (e.g. it matches `'that\'ll do'` but not `'\'hi\''`).
    Also regexes are actually not too slow if compiled in release mode. I'll
    probably get them working before trying to optimize w/ nom (also I don't
    even know how fast nom will be anyway).
    I might also try a lalrpop grammar.
    But for now, we're going to stick with the slow-ish regexes.
    The next problem is fixing/handling some invalid utf8 in the dump (?)

--- parse/src/lib.rs
+++ parse/src/lib.rs
@@ -1,10 +1,18 @@
+#![feature(test)]
+
 use std::io::{BufRead, BufReader};
 use std::fs::File;
+extern crate test;
+extern crate regex;
+
+#[macro_use] extern crate nom;
 
 #[cfg(test)]
 mod tests {
+    use regex::Regex;
     use std::io::{BufRead, BufReader, Read};
     use std::fs::File;
+    use test::Bencher;
 
     #[test]
     fn it_works() {
@@ -17,11 +25,38 @@ mod tests {
         }
     }
 
+    #[bench]
+    fn line_regex(b: &mut Bencher) {
+        let filepath = "/home/owen/shared/code/rust/wikilinks/one_line";
+        let mut f = File::open(filepath).unwrap();
+        let re = Regex::new(r"\((\d)+,-?\d+,'([^'\\]*(?:\\.[^'\\]*)*)',-?\d+\)").unwrap();
+        b.iter(|| {
+            let mut s = String::new();
+            f.read_to_string(&mut s);
+            let mut count = 0;
+            let m = re.captures_iter(&s);
+            for c in m {
+                let _dst = c.at(2).unwrap();
+                let _src: u32 = c.at(1).unwrap().parse().unwrap();
+                count += 1;
+            }
+            println!("Count: {}", count);
+            //29465
+        })
+    }
+
     #[test]
     fn read_and_do_nothing() {
+        // English  Wiki Pagelinks: 37k lines, 38G bytes, qj-deb-serv
         //line by line BR w/ printing every 1k: 82.5 minutes, 37468 lines
-        //byte by byte BR w/ no printing: 
-        let filepath = "/home/qj/wikidata/enwiki-20161201-pagelinks.sql";
+        //byte by byte BR w/ no printing: 182 minutes, 38,132,820,826 bytes
+        //
+        // Simple Wiki Pagelinks: 314 lines, 264M bytes, stenso-deb-laptop
+        //
+        //byte by byte BR: 70 seconds
+        //
+        //let filepath = "/home/qj/wikidata/enwiki-20161201-pagelinks.sql";
+        let filepath = "/home/owen/shared/code/rust/wikilinks/simplewiki-20161201-pagelinks.sql";
         let f = File::open(filepath).unwrap();
         let r = BufReader::new(f);
         //let mut v = Vec<u8>::with_capacity(10_000);
@@ -29,7 +64,7 @@ mod tests {
             //if i%1000 == 0 {
             //    println!("\t{}", i);
             //}
-        let mut bytes = 0u64;    
+        let mut bytes = 0u64;
         for b in r.bytes() {
             bytes += 1;
         }
@@ -111,3 +146,43 @@ fn parse_pagelinks_mysql(r: BufReader<File>) {
     println!("Count: {}", count);
 }
 */
+
+use std::borrow::Cow;
+
+#[derive(Debug)]
+struct Link<'a> {
+    src: u32,
+    dst: Cow<'a, str>,
+}
+
+pub fn str_to_u32(b: &[u8]) -> u32 {
+    //only accepts [0-9] (no commas, no negatives)
+    use std::str::from_utf8;
+    let s = from_utf8(b).unwrap();
+    u32::from_str_radix(s, 10).unwrap()
+}
+
+fn parse_pagelinks_nom(r: BufReader<File>) {
+    use nom::{IResult, digit};
+
+    named!(title, escaped!(is_not!("\\'"), '\\', is_a!("\"n\\")));
+    named!(pos_num <&[u8], u32>, map!(recognize!(digit), str_to_u32));
+    named!(link(&[u8]) -> Link, do_parse!(
+            tag!("(")   >>
+            recognize!(digit) >>
+            tag!(",")   >>
+            x: pos_num  >>
+            tag!(",")   >>
+            y: title    >>
+            tag!(",")  >>
+            recognize!(digit) >>
+            tag!(")")   >>
+            ( Link{ src:x, dst: String::from_utf8_lossy(y) } )
+            ));
+
+    for line in r.lines() {
+        let l = line.unwrap();
+    }
+}
+
+
