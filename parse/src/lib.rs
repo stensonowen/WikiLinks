38ef949bf503a2b8b8f4c1999801d7a3e6e165b1
owen <stensonowen@gmail.com>
2016-12-26 20:21:59 -0500

    verify everything parses to the right type first

--- parse/src/lib.rs
+++ parse/src/lib.rs
@@ -14,13 +14,20 @@ extern crate regex;
 
 /* Process:
  *  0   run ./retrieve.sh to download/gunzip everything
- *  1   read through *page.sql to map every page_id to an article object
+ *  1   read through *page.sql to map every page_id to an article object (and back)
  *  2   read through *redirect.sql to mark redirects
  *  3   read through *pagelinks.sql to make note of every child link (in both directions?)
  *  4   output the entire thing into a format that `phc` likes
  */
 
-fn parse_file() {
+//bytes in the buffer for reading one line at a time
+//problems may arise if this buffer fills up all the way: some data will not be read
+//to be safe, it is ~20% larger than the longest line in a dump ( 1,025,987 - 1,039,069 )
+const BUFFER_SIZE: usize = 1_250_000;
+
+
+
+fn _parse_file() {
     let filename = "/home/owen/shared/code/rust/wikilinks/data/simplewiki-20161201-page.sql";
     println!("Opening `{}`...", filename);
     let f = File::open(filename).unwrap();
@@ -30,21 +37,27 @@ fn parse_file() {
 
 pub fn parse_redirects_regex_lossy(mut r: BufReader<File>) {
     //matches all 9278254 english wiki entries
+    //matches all   58130  simple wiki entries
     let page_id     = r"(\d+)";
     let page_nmsp   = r"(-?\d+)";   //namespace can be negative?
     let page_title  = r"'([^'\\]*(?:\\.[^'\\]*)*)'"; 
-    let page_iw     = r"('.*?')|NULL";  //can be but never has been NULL (slowdown: ~30%)
-    let page_frag   = r"('.*?')|NULL";
+    let page_iw     = r"(?:'.*?'|NULL)";  //can be but never has been NULL (slowdown: ~30%)
+    let page_frag   = r"(?:'.*?'|NULL)";
     let re_body = vec![page_id, page_nmsp, page_title, page_iw, page_frag];
     let re_query = format!(r"\({}\)", re_body.join(","));
     let re = regex::Regex::new(&re_query).unwrap();
-    let mut buffer = Vec::<u8>::with_capacity(1_250_000);
+    let mut buffer = Vec::<u8>::with_capacity(BUFFER_SIZE);
     let mut count = 0usize;
     while r.read_until(b'\n', &mut buffer).unwrap() > 0 {
         {
             let s: Cow<str> = String::from_utf8_lossy(&buffer);
             let m = re.captures_iter(&s);
-            count += m.count();
+            for c in m {
+                let _src_id: u32 = c.at(1).unwrap().parse().unwrap();
+                let _src_ns: i16 = c.at(2).unwrap().parse().unwrap();
+                let _dst = c.at(3).unwrap();
+                count += 1;
+            }
         }
         buffer.clear();
     }
@@ -54,25 +67,22 @@ pub fn parse_redirects_regex_lossy(mut r: BufReader<File>) {
 }
 
 pub fn parse_pagelinks_regex_lossy(mut r: BufReader<File>) {
-    //like parse_pagelinks_regex but can tolerate occasional utf-16 characters
-    // which are extremely uncommon but not non-existant in the pagelinks dump
-    //Also I think this should be faster
-    //When built in release mode we find 1022340437 links from 38GB in 90 
+    //finds all 1022340437 pagelinks in english wiki in 90 mins (38GB)
+    //finds all    8730859 pagelinks in  simple wiki
     
-    //our buffer size is ~20% greater than the longest line we've found
-    let mut buffer = Vec::<u8>::with_capacity(1_250_000);
+    let mut buffer = Vec::<u8>::with_capacity(BUFFER_SIZE);
 	
-    //keep track of all the matching links we find
     let mut count = 0u64;
-    let re = regex::Regex::new(r"\((\d)+,-?\d+,'([^'\\]*(?:\\.[^'\\]*)*)',-?\d+\)").unwrap();
+    let re = regex::Regex::new(r"\((\d)+,(-?\d+),'([^'\\]*(?:\\.[^'\\]*)*)',-?\d+\)").unwrap();
 
     while r.read_until(b'\n', &mut buffer).unwrap() > 0 {
         {
             let s: Cow<str> = String::from_utf8_lossy(&buffer);
             let m = re.captures_iter(&s);
-            for _c in m {
-                //let dst: &str = c.at(2).unwrap();
-                //let src: u32  = c.at(1).unwrap().parse().unwrap();
+            for c in m {
+                let _src_id: u32 = c.at(1).unwrap().parse().unwrap();
+                let _src_ns: i16 = c.at(2).unwrap().parse().unwrap();
+                let _dst: &str = c.at(3).unwrap();
                 count += 1;
             }
         }
@@ -83,12 +93,10 @@ pub fn parse_pagelinks_regex_lossy(mut r: BufReader<File>) {
 
 pub fn parse_pages_regex_lossy(mut r: BufReader<File>, is_simple: bool) {
     //successfully locates all 408784 entries in the simple and 40966811 in the english
-    //      simple  = 408739 + 45 = 408784
-    //      english = 40962071 + 4740 = 40966811 == 40966811
 
 	// we make a few assumptions here; matches everything in the english page.sql dump
     let page_id     = r"(\d+)";     //captured, positive non-null number
-    let page_nmsp   = r"(\d+)";     //captured; should never be negative (0-15 ∪ 1000-2**31)
+    let page_nmsp   = r"(-?\d+)";   //captured; should never be negative(?) (0-15 ∪ 1000-2**31)
     let page_title  = r"'([^'\\]*(?:\\.[^'\\]*)*)'"; //surrounded by `'`s, which can be escaped
     let page_restrs = r"'.*?'"; 	//not always empty, but never has escaped quotes
     let page_counter= r"\d+";       //non-captured positive number; count will be wrong 
@@ -109,28 +117,25 @@ pub fn parse_pages_regex_lossy(mut r: BufReader<File>, is_simple: bool) {
         page_ln_upd, page_latest,   page_len, 
        /*page_ntc,*/ page_cont_md,  page_lang
     ];
-    if is_simple {
-        re_body.insert(12, page_ntc);
-    }
+    if is_simple { re_body.insert(12, page_ntc); }
 
     let re_query = format!(r"\({}\)", re_body.join(","));
     let re = regex::Regex::new(&re_query).unwrap();
-
-    let mut buffer = Vec::<u8>::with_capacity(1_250_000);
-	
-    //keep track of all the matching links we find
+    let mut buffer = Vec::<u8>::with_capacity(BUFFER_SIZE);
     let mut count = 0usize;
-
     while r.read_until(b'\n', &mut buffer).unwrap() > 0 {
         {
             let s: Cow<str> = String::from_utf8_lossy(&buffer);
             let m = re.captures_iter(&s);
-            count += m.count();
+            for c in m {
+                let _src_id: u32 = c.at(1).unwrap().parse().unwrap();
+                let _src_ns: i16 = c.at(2).unwrap().parse().unwrap();
+                let _dst = c.at(3).unwrap();
+                let _redr = c.at(4).unwrap() == "1";
+                count += 1;
+            }
         }
         buffer.clear();
     }
     println!("count: {}", count);
 }
-
-
-
--- parse/src/main.rs
+++ parse/src/main.rs
@@ -4,9 +4,14 @@ use std::fs::File;
 extern crate parse;
 
 fn main() {
-    let filename = "/home/owen/shared/code/rust/wikilinks/data/simplewiki-20161201-page.sql";
-    println!("Opening `{}`...", filename);
-    let f = File::open(filename).unwrap();
+    //let filename = "/home/owen/shared/code/rust/wikilinks/data/simplewiki-20161201-page.sql";
+    //let f = "/home/owen/shared/code/rust/wikilinks/data/simplewiki-20161201-pagelinks.sql";
+    //let f = "/home/owen/shared/code/rust/wikilinks/data/simplewiki-20161201-page.sql";
+    let f = "/home/owen/shared/code/rust/wikilinks/data/simplewiki-20161201-redirect.sql";
+    println!("Opening `{}`...", f);
+    let f = File::open(f).unwrap();
     let r = BufReader::new(f);
-    parse::parse_pages_regex_lossy(r, true);
+    //parse::parse_pagelinks_regex_lossy(r);
+    //parse::parse_pages_regex_lossy(r, true);
+    parse::parse_redirects_regex_lossy(r);
 }
