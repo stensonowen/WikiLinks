faf622f1841b28e4fa99fdc871cd3f1e0bcfd23a
owen <stensonowen@gmail.com>
2016-12-29 02:33:41 -0500

    rustfmt

--- parse/src/database.rs
+++ parse/src/database.rs
@@ -1,79 +1,88 @@
 extern crate regex;
 use std::collections::HashMap;
 
-//helpers
+// helpers
 
-//Anything a page_id can represent
+// Anything a page_id can represent
 #[derive(Debug)]
 enum Entry {
-    //either a unique page and its link data
-    Page { title: String, children: Vec<u32>, parents: Vec<u32> },
+    // either a unique page and its link data
+    Page {
+        title: String,
+        children: Vec<u32>,
+        parents: Vec<u32>,
+    },
     // or the page_id of what it redirects to (which might not be known yet)
     Redirect(Option<u32>),
 }
 
 impl Entry {
     fn new_page(n: &str) -> Entry {
-        Entry::Page { title: String::from(n), children: Vec::new(), parents: Vec::new() }
+        Entry::Page {
+            title: String::from(n),
+            children: Vec::new(),
+            parents: Vec::new(),
+        }
     }
 }
 
-//The id numbers associated with an article title
+// The id numbers associated with an article title
 #[derive(Debug)]
 enum Address {
-    //Can be its true page_id
+    // Can be its true page_id
     PageId(u32),
-    //Or can be a list of redirects
-    //Once the true page_id is known, though, all redirects (and this) must be updated
+    // Or can be a list of redirects
+    // Once the true page_id is known, though, all redirects (and this) must be updated
     Redirects(Vec<u32>),
 }
 
 
-//The actual data storing the internal link structure
+// The actual data storing the internal link structure
 pub struct Database {
     // when populating the entries/addresses fields, we may come across redirect pages
     // which means that multiple addresses (u32) can map to the same page
-    
+    //
     // Address  →  Page
-    entries: HashMap<u32,Entry>,
+    entries: HashMap<u32, Entry>,
     //  Title   →  Address
-    addresses: HashMap<String,Address>,
+    addresses: HashMap<String, Address>,
 }
 
 impl Database {
     pub fn new() -> Database {
-        Database{ 
-            entries:   HashMap::new(), 
+        Database {
+            entries: HashMap::new(),
             addresses: HashMap::new(),
         }
     }
-    pub fn add_page(&mut self, data: &regex::Captures) { 
+    pub fn add_page(&mut self, data: &regex::Captures) {
         let page_id: u32 = data.at(1).unwrap().parse().unwrap();
         let title = data.at(2).unwrap();
         let is_redr = data.at(3).unwrap();
         if is_redr == "1" {
-            //get a handle to the address this title points to
-            //if it's not already in our db, add it and make note of this redirect
-            let addr = self.addresses.entry(String::from(title))
-                                     .or_insert(Address::Redirects(vec![]));
-            //assuming it was already accounted for:
+            // get a handle to the address this title points to
+            // if it's not already in our db, add it and make note of this redirect
+            let addr = self.addresses
+                .entry(String::from(title))
+                .or_insert(Address::Redirects(vec![]));
+            // assuming it was already accounted for:
             // if we know its address, point page_id's entry at it
             // otherwise, add page_id to its list of redirects that must be updated
 
-            if let &mut Address::PageId(true_id) = addr { 
+            if let &mut Address::PageId(true_id) = addr {
                 // `true_id -> title` is already in the database
                 self.entries.insert(page_id, Entry::Redirect(Some(true_id)));
             } else if let &mut Address::Redirects(ref mut v) = addr {
                 // other pages pages have already tried to redirect to `title`
                 v.push(page_id);
             } else {
-                //we've never seen `title` before, so we added addrs[title] = Redir([id])
+                // we've never seen `title` before, so we added addrs[title] = Redir([id])
                 self.entries.insert(page_id, Entry::Redirect(None));
             }
         } else {
-            //this is not a redirect; it's a real article
-            //check if we've already seen any redirects that point to this title
-            //if we've never seen it before, just add the page_id and be done
+            // this is not a redirect; it's a real article
+            // check if we've already seen any redirects that point to this title
+            // if we've never seen it before, just add the page_id and be done
             if let Some(&Address::Redirects(ref v)) = self.addresses.get(title) {
                 //entry had redirects we have to deal with; set their destinations.
                 //`v` is a list of addresses which are of type Entry::Redirect(None)
@@ -82,38 +91,41 @@ impl Database {
                     self.entries.insert(*redirect, Entry::Redirect(Some(page_id)));
                 }
             }
-            //now insert this valid address/article into the tables
+            // now insert this valid address/article into the tables
             self.addresses.insert(String::from(title), Address::PageId(page_id));
             self.entries.insert(page_id, Entry::new_page(title));
         }
     }
-    pub fn add_redirect(&mut self, data: &regex::Captures) { 
-        panic!("TODO: get rid of this code segment (I think)");
-        //let src_id: u32 = data.at(1).unwrap().parse().unwrap();
-        //let dst: &str = data.at(2).unwrap();
-        //if let Some(dst_id) = self.addresses.get(dst) {
-        //    //redirects to something we care about
-        //    self.redirects.insert(src_id, Some(*dst_id));
-        //}
-    }
+    // pub fn add_redirect(&mut self, data: &regex::Captures) {
+    // panic!("TODO: get rid of this code segment (I think)");
+    // let src_id: u32 = data.at(1).unwrap().parse().unwrap();
+    // let dst: &str = data.at(2).unwrap();
+    // if let Some(dst_id) = self.addresses.get(dst) {
+    //    //redirects to something we care about
+    //    self.redirects.insert(src_id, Some(*dst_id));
+    //
+    // }
+    //
     pub fn add_pagelink(&mut self, data: &regex::Captures) {
         let src_id: u32 = data.at(1).unwrap().parse().unwrap();
         let dst: &str = data.at(2).unwrap();
 
         if let Some(&Address::PageId(dst_id)) = self.addresses.get(dst) {
-            //it is not uncommon (>20%) for the destination title to be absent
-            //I think this means there are broken links / links to other wikis / namespaces
-            //pretty sure they should just be discarded
-            
-            if let Some(&mut Entry::Page{children: ref mut c,..}) = self.entries.get_mut(&src_id) {
-                //add parent-to-child link
+            // it is not uncommon (>20%) for the destination title to be absent
+            // I think this means there are broken links / links to other wikis / namespaces
+            // pretty sure they should just be discarded
+
+            if let Some(&mut Entry::Page { children: ref mut c, .. }) =
+                self.entries.get_mut(&src_id) {
+                // add parent-to-child link
                 c.push(dst_id);
             } else {
                 // src id was not found, then there's nothing left to do
                 return;
             }
-            if let Some(&mut Entry::Page{parents: ref mut p, ..}) = self.entries.get_mut(&dst_id) {
-                //add child-to-parent link
+            if let Some(&mut Entry::Page { parents: ref mut p, .. }) =
+                self.entries.get_mut(&dst_id) {
+                // add child-to-parent link
                 p.push(src_id);
             }
         }
@@ -123,8 +135,8 @@ impl Database {
         let mut originals = 0;
         let mut redir_some = 0;
         let mut redir_none = 0;
-        for (_,entry) in &self.entries {
-            if let &Entry::Page{ children: ref c, parents: ref p, ..} = entry {
+        for (_, entry) in &self.entries {
+            if let &Entry::Page { children: ref c, parents: ref p, .. } = entry {
                 children += c.len();
                 originals += 1;
             } else if let &Entry::Redirect(r) = entry {
@@ -142,11 +154,9 @@ impl Database {
         println!("Number of Real Entries: {}", originals);
         println!("Number of Redirects: {}", redir_some + redir_none);
         println!(" Number of redirects with no destination: {}", redir_none);
-        //println!(" Entries: `{:?}`", self.entries);
-        //println!(" Addresses: `{:?}`", self.addresses);
+        // println!(" Entries: `{:?}`", self.entries);
+        // println!(" Addresses: `{:?}`", self.addresses);
         println!("=============================================");
 
     }
 }
-
-
--- parse/src/lib.rs
+++ parse/src/lib.rs
@@ -7,66 +7,68 @@ mod regexes;
 mod database;
 use database::*;
 
-/* Parsing Note:
- *  Most of the [u8] -> &str conversions involve potential errors in which the source
- *  might not be valid u8. This is not common but the case must be handled.
- *  I'm not sure how utf-16 works with rust regexes, and it might mean lots more memory.
- *  We handle this with String::from_utf8_lossy(), which replaces bad utf8 with '�'
- *  It also returns a Cow, which suits our purposes well.
- */
+// Parsing Note:
+//  Most of the [u8] -> &str conversions involve potential errors in which the source
+//  might not be valid u8. This is not common but the case must be handled.
+//  I'm not sure how utf-16 works with rust regexes, and it might mean lots more memory.
+//  We handle this with String::from_utf8_lossy(), which replaces bad utf8 with '�'
+//  It also returns a Cow, which suits our purposes well.
+//
 
-/* Process:
- *  0   run ./retrieve.sh to download/gunzip everything
- *  1   read through *page.sql to map every page_id to an article object (and back)
- *  2   read through *redirect.sql to mark redirects
- *  3   read through *pagelinks.sql to make note of every child link (in both directions?)
- *  4   output the entire thing into a format that `phc` likes
- */
+// Process:
+//  0   run ./retrieve.sh to download/gunzip everything
+//  1   read through *page.sql to map every page_id to an article object (and back)
+//  2   read through *redirect.sql to mark redirects
+//  3   read through *pagelinks.sql to make note of every child link (in both directions?)
+//  4   output the entire thing into a format that `phc` likes
+//
 
-/* Used to be: addresses = entries = 172,350    / 408,784
- *
- */
+// Used to be: addresses = entries = 172,350    / 408,784
+//
+//
 
-//bytes in the buffer for reading one line at a time
-//problems may arise if this buffer fills up all the way: some data will not be read
-//to be safe, it is ~20% larger than the longest line in a dump ( 1,025,987 - 1,039,069 )
+// bytes in the buffer for reading one line at a time
+// problems may arise if this buffer fills up all the way: some data will not be read
+// to be safe, it is ~20% larger than the longest line in a dump ( 1,025,987 - 1,039,069 )
 const BUFFER_SIZE: usize = 1_250_000;
 
 
 pub fn populate_db() -> Database {
     let dir = String::from("/home/owen/shared/code/rust/wikilinks/data/");
-    //let pages_f = dir.clone() + "simplewiki-20161201-page.sql";
+    // let pages_f = dir.clone() + "simplewiki-20161201-page.sql";
     let pages_f = dir.clone() + "test-page.sql";
     let links_f = dir.clone() + "simplewiki-20161201-pagelinks.sql";
     let redir_f = dir + "simplewiki-20161201-redirect.sql";
 
     let mut db = Database::new();
-    let pages = parse_generic(&pages_f, &regexes::pages_regex(), &mut db, 
-                              |db: &mut Database, data: regex::Captures| { 
-                                  db.add_page(&data); 
+    let pages = parse_generic(&pages_f,
+                              &regexes::pages_regex(),
+                              &mut db,
+                              |db: &mut Database, data: regex::Captures| {
+                                  db.add_page(&data);
                               });
     println!("Number of page entries: {}", pages);
-    //let links = parse_generic(&links_f, &regexes::pagelinks_regex(), &mut db, 
-    //                          |db: &mut Database, data: regex::Captures| { 
-    //                              db.add_pagelink(&data); 
+    // let links = parse_generic(&links_f, &regexes::pagelinks_regex(), &mut db,
+    //                          |db: &mut Database, data: regex::Captures| {
+    //                              db.add_pagelink(&data);
     //                          });
-    //println!("Number of pagelinks: {}", links);
-    //let redirects = parse_generic(&redir_f, &regexes::redirect_regex(), &mut db, 
-    //                              |db: &mut Database, data: regex::Captures| { 
-    //                                  db.add_redirect(&data); 
+    // println!("Number of pagelinks: {}", links);
+    // let redirects = parse_generic(&redir_f, &regexes::redirect_regex(), &mut db,
+    //                              |db: &mut Database, data: regex::Captures| {
+    //                                  db.add_redirect(&data);
     //                              });
-    //println!("Number of redirects: {}", redirects);
-    
-    //println!("Number of elements: {}", db.len());
+    // println!("Number of redirects: {}", redirects);
+
+    // println!("Number of elements: {}", db.len());
     db
 }
 
 pub fn parse_generic<F>(filename: &str, re: &str, db: &mut Database, action: F) -> u64
     where F: Fn(&mut Database, regex::Captures) -> ()
 {
-    //parse a mysql dump from a custom regex
-    //use a closure to define how the database uses the results
-    
+    // parse a mysql dump from a custom regex
+    // use a closure to define how the database uses the results
+
     let f = File::open(filename).unwrap();
     let mut reader = BufReader::new(f);
     let re = regex::Regex::new(re).unwrap();
@@ -74,13 +76,15 @@ pub fn parse_generic<F>(filename: &str, re: &str, db: &mut Database, action: F) 
     let mut count = 0u64;
 
     loop {
-        let len = reader.read_until(b'\n', &mut buffer).unwrap(); 
+        let len = reader.read_until(b'\n', &mut buffer).unwrap();
         if len == 0 {
-            //done (empty line has a length of 1)
+            // done (empty line has a length of 1)
             break;
         } else if len == BUFFER_SIZE {
-            //crash/complain w/ error message
-            panic!(format!("Input db line was longer than your buffer size; increase BUFFER_SIZE to at least {}.", len));
+            // crash/complain w/ error message
+            panic!(format!("Input db line was longer than your buffer size; increase \
+                            BUFFER_SIZE to at least {}.",
+                           len));
         } else {
             let s: Cow<str> = String::from_utf8_lossy(&buffer);
             for entry in re.captures_iter(&s) {
@@ -92,5 +96,3 @@ pub fn parse_generic<F>(filename: &str, re: &str, db: &mut Database, action: F) 
     }
     count
 }
-
-
