abbb17f2de3ca1150582457f3f070b904ed34a4e
owen <stensonowen@gmail.com>
2017-01-05 05:15:05 -0500

    Pagerank headache
    Can't figure out why this iterative approach isn't working (sum of
    pageranks diverges (!)). As far as I can tell, the linear algebra
    implementation (which most literature references) requires a matrix of
    links... but that requires a N*N matrix, which really doesn't work when
    N is ~13 million. (I'm pretty sure there are simplifying shortcuts, but
    I don't think I know enough linear algebra to know what they are.)
    
    And for the life of me I can't find where my solution is wrong.
    Web::iterate3() simplifies the problem by using a damping factor of 1
    (i.e. ignoring the damping factor), meaning the surfer will never stop
    (?). This had confusingly positive results, as the (in the simple wiki)
    the sum of the pageranks stayed at 1 and the maximum difference seemed
    to converge.
    
    Also, I'm at least entertaining the thought of using an arbitrary
    precision library to represent pageranks instead of f64s. But that would
    almost certainly have a steep runtime cost. Meh.

--- pagerank/src/lib.rs
+++ pagerank/src/lib.rs
@@ -1,6 +1,5 @@
 extern crate wikidata;
 extern crate phf;
-extern crate ramp;
 
 use wikidata::ENTRIES as ENTRIES;
 //use wikidata::ADDRESSES as ADDRESSES;
@@ -50,12 +49,9 @@ impl Web2 {
 }
 */
 
-use ramp::int::Int;
-use ramp::rational::Rational;
 
 pub struct Web {
     pages:  &'static phf::Map<u32, wikidata::Page>,
-    //ranks:  HashMap<u32,Rational>,
     ranks:  HashMap<u32,f64>,
     iter:   usize,
 }
@@ -64,10 +60,9 @@ impl Web {
     pub fn new() -> Web {
         let size = ENTRIES.len();
         let mut pageranks = HashMap::with_capacity(size);
-        //let guess = (size as f64).recip();  // start each pagerank at 1/N
-        let guess = Rational::new(Int::one(), Int::from(size));
+        let guess = (size as f64).recip();  // start each pagerank at 1/N
         for &entry in ENTRIES.keys() {
-            pageranks.insert(entry,guess.clone());
+            pageranks.insert(entry,guess);
         }
         Web {
             pages:  &ENTRIES,
@@ -75,55 +70,41 @@ impl Web {
             iter:   0,
         }
     }
-    //pub fn sum(&self) -> f64 {
-    pub fn sum(&self) -> Rational {
-        //let mut sum = 0f64;
-        let mut sum = Rational::new(Int::zero(), Int::one());
+    pub fn sum(&self) -> f64 {
+        let mut sum = 0f64;
         for addr in self.pages.keys() {
-            sum += self.ranks.get(addr).unwrap()
+            sum += *self.ranks.get(addr).unwrap()
         }
         sum
     }
-    //pub fn iterate(&mut self) -> f64 {
-    pub fn iterate(&mut self) -> Rational {
+    pub fn iterate(&mut self) -> f64 {
         //all new pageranks will be calculated from the duplicated values (the old table)
         //return the maximum change in an article's pagerank
-        //let mut new_ranks: HashMap<u32,Int> = HashMap::with_capacity(self.ranks.capacity());
-        let mut new_ranks: HashMap<u32,Rational> = HashMap::with_capacity(self.ranks.capacity());
-        //let base_pr = (1f64 - DAMPING_FACTOR) / (self.pages.len() as f64);
-        //let base_pr = (1f64 - DAMPING_FACTOR) / (self.pages.len() as f64);
-        // (1-.85)/N = (3/20)/N = 3/(20*N)
-        let base_pr = Rational::new(Int::from(3),Int::from(20*self.pages.len()));
-        //let mut max_change = 0f64;
-        let mut max_change = Rational::new(Int::zero(), Int::one());
-        //let mut sink_pr = 0f64;
-        let mut sink_pr = Rational::new(Int::zero(), Int::one());
+        let mut new_ranks: HashMap<u32,f64> = HashMap::with_capacity(self.ranks.capacity());
+        let base_pr = (1f64 - DAMPING_FACTOR) / (self.pages.len() as f64);
+        let mut max_change = 0f64;
+        let mut sink_pr = 0f64;
         for (addr,page) in self.pages.into_iter() {
             if page.children.len() == 0 {
-                sink_pr += self.ranks.get(addr).unwrap()
+                sink_pr += *self.ranks.get(addr).unwrap()
             }
         }
         for (&addr,page) in self.pages.into_iter() {
             //track sum of `page`'s parents's pageranks divided by their number of children
-            //let mut extra_pr = 0f64;
-            let mut extra_pr = Rational::new(Int::zero(), Int::one());
+            let mut extra_pr = 0f64;
             for p in page.parents {
                 let parent_pr = self.ranks.get(p).unwrap();
-                //let parent_num_kids = self.pages.get(p).unwrap().children.len() as f64;
-                let parent_num_kids = Int::from(self.pages.get(p).unwrap().children.len());
+                let parent_num_kids = self.pages.get(p).unwrap().children.len() as f64;
                 //parent_num_kids should always be ≥1: our parent has at least 1 kid (us)
                 extra_pr += parent_pr / parent_num_kids;
             }
             //calculate our pagerank: (1-d)/N + d*Σ(PR(p)/L(p))
             //let pr = base_pr + DAMPING_FACTOR * extra_pr;
             //sink_pr = DAMPING_FACTOR * sink_pr / (self.pages.len() as f64); 
-            //sink_pr = sink_pr / (self.pages.len() as f64); 
-            sink_pr = sink_pr.clone() / Int::from(self.pages.len());
-            let pr = base_pr.clone() 
-                + Rational::new(Int::from(17),Int::from(20)) * 
-                (extra_pr.clone() + sink_pr.clone());
+            sink_pr = sink_pr / (self.pages.len() as f64); 
+            let pr = base_pr + DAMPING_FACTOR * (extra_pr + sink_pr);
             //keep track of the magnitude of the changes made
-            let change = (self.ranks.get(&addr).unwrap() - pr.clone()).abs();
+            let change = (self.ranks.get(&addr).unwrap() - pr).abs();
             if change > max_change {
                 max_change = change;
             }
@@ -135,6 +116,68 @@ impl Web {
         max_change
     }
     pub fn iterate2(&mut self) -> f64 {
-
+        //returns the max difference
+        let mut new_ranks: HashMap<u32,f64> = HashMap::new();
+        //each page transfers its pagerank equally among its children
+        //unless it has no children, then it transfers equally among all pages
+        let mut max_change = 0.;
+        
+        for (addr,page) in self.pages.into_iter() {
+            let mut pr = 0.;
+            for p in page.parents {
+                let parent_rank = self.ranks.get(p).unwrap();
+                let num_siblings = self.pages.get(p).unwrap().children.len() as f64;
+                pr += parent_rank / num_siblings;
+            }
+            pr = 1. - DAMPING_FACTOR + DAMPING_FACTOR * pr;
+            new_ranks.insert(*addr,pr);
+            let delta = (pr - self.ranks.get(addr).unwrap()).abs();
+            if delta > max_change {
+                max_change = delta;
+            }
+        }
+        self.ranks = new_ranks;
+        max_change
+    }
+    pub fn iterate3(&mut self) -> f64 {
+        /* wtf
+         *  This simplified method is slower but seems more correct (?!)
+         *  We ignore the damping factor and square our cache misses (so it's defs slower), 
+         *   but our total sum osciallates around 1.000... and our max diff converges
+         *  But we totally ignore the concept of a damping factor so this can't be right
+         *  There should be a constant starting value for pageranks (instead of `0.`) and
+         *   the influence a page has should decrease by 15% every iteration (right???)
+         *  Why does this behave like iterate1 and iterate2 should?
+         */
+        let mut new_ranks: HashMap<u32,f64> = HashMap::with_capacity(self.ranks.capacity());
+        //each page transfers its pagerank equally among its children
+        //unless it has no children, then it transfers equally among all pages
+        for (addr,page) in self.pages.into_iter() {
+            let pr = self.ranks.get(addr).unwrap();
+            if page.children.len() == 0 {
+                //equally distribute our pagerank to every page
+                let n = self.pages.len() as f64;
+                for &a in self.pages.keys() {
+                    let mut x = new_ranks.entry(a).or_insert(0.);
+                    *x += pr / n;
+                }
+            } else {
+                //equally distribute our pagerank to all our children
+                let n = page.children.len() as f64;
+                for &a in page.children {
+                    let mut x = new_ranks.entry(a).or_insert(0.);
+                    *x += pr / n;
+                }
+            }
+        }
+        let mut max_change = 0f64;
+        for (addr,rank) in &self.ranks {
+            let delta = (rank - new_ranks.get(addr).unwrap()).abs();
+            if delta > max_change {
+                max_change = delta;
+            }
+        }
+        self.ranks = new_ranks;
+        max_change
     }
 }
--- pagerank/src/main.rs
+++ pagerank/src/main.rs
@@ -1,5 +1,3 @@
-//extern crate wikidata;
-//use wikidata::ENTRIES;
 extern crate pagerank;
 extern crate wikidata;
 
@@ -8,40 +6,7 @@ fn main() {
     println!("Initial Sum:  {}", web.sum());
     println!(" i\t\tMax Diff\t\tSum");
     for i in 0..20 {
-        let diff = web.iterate();
+        let diff = web.iterate3();
         println!("{:03}:    {}, \t{}", i, diff, web.sum());
     }
-    /*
-	println!("Length of entries: {}", wikidata::ENTRIES.len());
-	println!("Length of addresses: {}", wikidata::ADDRESSES.len());
-
-	if let Some(x) = wikidata::ADDRESSES.get("Rust") {
-		println!("addrs['Rust'] = {}", x);
-		if let Some(&wikidata::Page { title: t, children: c, parents: p }) 
-				= wikidata::ENTRIES.get(&x) {
-			println!("entries[{}]:", x);
-			println!("\ttitle:\t`{}`", t);
-            println!("\tchildren:");
-            for i in c {
-                if let Some(&wikidata::Page { title: ref y, .. }) = wikidata::ENTRIES.get(i) {
-                    println!("\t\t`{}`", y);
-                } else {
-                    println!("\t\tERROR: NOT FOUND");
-                }
-            }
-            println!("\tparents:");
-            for i in p {
-                if let Some(&wikidata::Page { title: ref y, .. }) = wikidata::ENTRIES.get(i) {
-                    println!("\t\t`{}`", y);
-                } else {
-                    println!("\t\tERROR: NOT FOUND");
-                }
-            }
-		} else {
-			println!("Couldn't find {}", x);
-		}
-	} else {
-		println!("Couldn't find `Rust`");
-	}
-    */
 }
