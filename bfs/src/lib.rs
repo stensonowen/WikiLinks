af15b5287f2a24e4c98ce7cef3534f4995485c8f
owen <stensonowen@gmail.com>
2017-01-10 01:19:14 -0500

    Refactor bfs; add cli functionality

--- bfs/src/lib.rs
+++ bfs/src/lib.rs
@@ -1,92 +1,172 @@
 extern crate wikidata;
 extern crate phf;
+extern crate ironstorm_lookup;
+#[macro_use]
+extern crate lazy_static;
 
 use std::collections::{HashSet, HashMap};
-use wikidata::ENTRIES;
+use wikidata::{ADDRESSES, ENTRIES};
 use std::mem::swap;
 
-const MAX_DEPTH: usize = 5;
-//const START_SIZE: usize = 100;  
+const MAX_DEPTH: usize = 10;
+const SEARCH_RESULTS: usize = 10;
 
-pub fn bfs(src: u32, dst: u32) -> Result<Vec<u32>,usize> {
-    //perform breadth-first-search using wikidata::ENTRIES from src to dst
-    //return vector of page_id's (i.e. the path)
-    //should this be multithreaded??
-    //TODO: investigate using a Bloom Filter for checking intersection quickly
-    //  Might be faster: that is its thing
-    //  Might be negligible: uses hashing and a lookup table, which is basically a hashmap
-    //TODO: play with different approximate hints 
-    //  i.e. starting/changing hashmap capacity via guessing
-    //TODO: fix code duplication (top-down and bottom-up doing almost the same thing
-    //TODO: use fewer than SIX hashmaps?
-    //TODO: there's a fair bit of redundancy: top_down_n ⊆ src_seen ⊇ top_down_o
-    
-    //`seen` pages: the id we've encountered and the id that linked to it (parent OR child)
-    let mut src_seen: HashMap<u32,u32> = HashMap::new();
-    let mut dst_seen: HashMap<u32,u32> = HashMap::new();
+use std::iter::FromIterator;
+use ironstorm_lookup::{LookupTable, Lookup, Bucket};
 
-    //keep track of articles we're checking in each direction
-    //each direction should have an immutable bank and a mutable collector (that swap)
-    //start from src, follow children
-    let mut top_down_n:  HashSet<u32> = HashSet::new();
-    let mut top_down_o:  HashSet<u32> = HashSet::new();
-    top_down_o.insert(src);
-    //start from dst, follow parents
-    let mut bottom_up_n: HashSet<u32> = HashSet::new();
-    let mut bottom_up_o: HashSet<u32> = HashSet::new();
-    bottom_up_o.insert(dst);
 
-    for _ in 0 .. MAX_DEPTH {
-        //top-down first? Any benefit to doing both at the same time?
-        for &p in &top_down_o {
-            for &c in ENTRIES.get(&p).unwrap().children {
-                //for each child, if unseen, mark it as interest
-                if *src_seen.entry(c).or_insert(p) == p {
-                    //a little wonky but I think slightly more efficient
-                    // if `c` hasn't been seen, insert `seen[c]=p` and skip ahead 
-                    // otherwise, `c` has been inserted into `seen`
-                    //if `c` was unseen, check if it has been found
-                    if dst_seen.contains_key(&c) {
-                        return Ok(extract_path(src, dst, &src_seen, &dst_seen, c));
-                    }
-                    top_down_n.insert(c);
-                }
-            }
-        }
-        top_down_o.clear();
-        swap(&mut top_down_o, &mut top_down_n);
+// Locate article by title
 
-        //now do the same thing from the other direction
-        for &p in &bottom_up_o {
-            for &c in ENTRIES.get(&p).unwrap().parents {
-                if *dst_seen.entry(c).or_insert(p) == p {
-                    if src_seen.contains_key(&p) {
-                        return Ok(extract_path(src, dst, &src_seen, &dst_seen, c));
-                    }
-                    bottom_up_n.insert(c);
-                }
-            }
-        }
-        bottom_up_o.clear();
-        swap(&mut bottom_up_o, &mut bottom_up_n);
+struct Title(&'static str);
+
+impl Lookup for Title {
+    // do I have to use a dummy struct here? I don't think I can implement Lookup for &str
+    // How much of the `Title` struct will be optimized away?
+    fn searchable_text(&self) -> String {
+        self.0.to_string()
     }
-
-    Err(MAX_DEPTH)
+    fn bucket(&self) -> Bucket {
+        // optionally, use the title's pagerank ?
+        // use length: prioritize shorter entries
+        self.0.len()
+    }
 }
 
-fn extract_path(src: u32, dst: u32,
-                src_seen: &HashMap<u32,u32>, 
-                dst_seen: &HashMap<u32,u32>, 
-                common: u32) -> Vec<u32> {
-    //given entries findable from the src, those from the dst, and an intersecting entry,
+lazy_static! {
+    static ref TITLES: LookupTable<'static, Title> = {
+        let i = ADDRESSES.keys().map(|a| Title(a));
+        LookupTable::from_iter(i)
+    };
+}
+
+pub fn search(query: &str) -> Vec<&'static str> {
+    TITLES.find(query).take(SEARCH_RESULTS).map(|t| t.0).collect()
+}
+
+pub fn load_titles() {
+    TITLES.len();
+}
+
+
+// Find the shortest path between articles
+
+pub enum Error {
+    NoSuchPath,
+    Terminated(usize),
+}
+
+pub fn bfs(src: u32, dst: u32) -> Result<Vec<u32>, Error> {
+    // perform breadth-first-search using wikidata::ENTRIES from src to dst
+    // return vector of page_id's (i.e. the path)
+    // should this be multithreaded??
+    // TODO: investigate using a Bloom Filter for checking intersection quickly
+    //  Might be faster: that is its thing
+    //  Might be negligible: uses hashing and a lookup table, which is basically a hashmap
+    //  Might be useful if this were multithreaded?
+    // TODO: play with different approximate hints
+    //  i.e. starting/changing hashmap capacity via guessing
+    // TODO: fix code duplication (top-down and bottom-up doing almost the same thing
+    // TODO: there's a fair bit of redundancy: top_down_n ⊆ src_seen ⊇ top_down_o
+
+    if src == dst {
+        return Ok(vec![src, dst]);
+    }
+
+    //`seen` pages: the id we've encountered and the id that linked to it (parent OR child)
+    let mut src_seen: HashMap<u32, u32> = HashMap::new();
+    let mut dst_seen: HashMap<u32, u32> = HashMap::new();
+
+    // keep track of articles we're checking in each direction
+    // each direction should have an immutable bank and a mutable collector (that swap)
+    // start from src, follow children
+    let mut top_down: HashSet<u32> = HashSet::new();
+    top_down.insert(src);
+    // start from dst, follow parents
+    let mut bottom_up: HashSet<u32> = HashSet::new();
+    bottom_up.insert(dst);
+
+    let mut tmp: HashSet<u32> = HashSet::new();
+
+    for _ in 0..MAX_DEPTH {
+        if let Some(midpoint) = iterate(&top_down,
+                                        &mut tmp,
+                                        SearchDirection::Down,
+                                        &mut src_seen,
+                                        &dst_seen) {
+            return Ok(extract_path(src, dst, &src_seen, &dst_seen, midpoint));
+        }
+        top_down.clear();
+        swap(&mut top_down, &mut tmp);
+
+        if let Some(midpoint) = iterate(&bottom_up,
+                                        &mut tmp,
+                                        SearchDirection::Up,
+                                        &mut dst_seen,
+                                        &src_seen) {
+            return Ok(extract_path(src, dst, &src_seen, &dst_seen, midpoint));
+        }
+        bottom_up.clear();
+        swap(&mut bottom_up, &mut tmp);
+
+        if bottom_up.is_empty() || top_down.is_empty() {
+            return Err(Error::NoSuchPath);
+        }
+    }
+    Err(Error::Terminated(MAX_DEPTH))
+}
+
+enum SearchDirection {
+    Up,
+    Down,
+}
+
+fn iterate(prev_line: &HashSet<u32>,
+           new_line: &mut HashSet<u32>,
+           direction: SearchDirection,
+           reachable: &mut HashMap<u32, u32>,
+           targets: &HashMap<u32, u32>)
+           -> Option<u32> {
+    // go through every element in `prev_line`, adding parents/children to `next_line`
+    // as we see each entry, add it to `reachable`.
+    // If we find an element in `targets` and `reachable`, searching is over: return it
+    for &i in prev_line {
+        // link_start
+        let pool = match direction {
+            SearchDirection::Up => ENTRIES.get(&i).unwrap().parents,
+            SearchDirection::Down => ENTRIES.get(&i).unwrap().children,
+        };
+        for &j in pool {
+            // link_end
+            // for each link_end, if unseen, mark it as reachable
+            if *reachable.entry(j).or_insert(i) == i {
+                // we successfully inserted `seen[j]=i`, and it wasn't there before
+                if targets.contains_key(&j) {
+                    // we found an element that is in both `reachable` and `target`
+                    return Some(j);
+                }
+                new_line.insert(j);
+            }
+        }
+    }
+    None
+
+}
+
+fn extract_path(src: u32,
+                dst: u32,
+                src_seen: &HashMap<u32, u32>,
+                dst_seen: &HashMap<u32, u32>,
+                common: u32)
+                -> Vec<u32> {
+    // given entries findable from the src, those from the dst, and an intersecting entry,
     // find the path from src to dst
-    //For now, the path should include the src and dst ids
+    // For now, the path should include the src and dst ids
     let mut path = vec![common];
-    //first find the path from the midpoint to the src (will be backwards)
+    // first find the path from the midpoint to the src (will be backwards)
     let mut current = common;
     while current != src {
-        //could just iterate until it finds something without a parent? no passing src/dst ?
-        //this is easier to debug for now
+        // could just iterate until it finds something without a parent? no passing src/dst ?
+        // this is easier to debug for now
         current = *src_seen.get(&current).unwrap();
         path.push(current);
     }
@@ -99,14 +179,44 @@ fn extract_path(src: u32, dst: u32,
     path
 }
 
-pub fn print_path(res: Result<Vec<u32>,usize>) {
-    if let Ok(path) = res {
-        println!("Found a path with {} steps: ", path.len()-1);
-        for id in &path {
-            let title = ENTRIES.get(id).unwrap().title;
-            println!("\t{: >12}: \t`{}`", id, title);
-        }
-    } else if let Err(x) = res {
-        println!("Failed to find a path after {} iterations", x);
+pub fn print_path(res: Result<Vec<u32>, Error>, url: Option<&str>) {
+    match res {
+        Ok(path) => {
+            println!("Found a path with {} steps: ", path.len() - 1);
+            for id in &path {
+                let title = ENTRIES.get(id).unwrap().title;
+                if let Some(prefix) = url {
+                    println!("\t{: >10}: \t{: <16} (https://{}.wikipedia.org/?curid={: <10})",
+                             id,
+                             title,
+                             prefix,
+                             id);
+                } else {
+                    println!("\t{: >12}: \t`{}`", id, title);
+                }
+            }
+        },
+        Err(Error::Terminated(t)) => println!("The search was terminated after {} rounds", t),
+        Err(Error::NoSuchPath) => println!("No such path of any length exists"), 
+    }
+}
+
+pub fn format_path(res: Result<Vec<u32>, Error>, lang: &str) -> String {
+    //output html
+    match res {
+        Ok(path) => {
+            let mut s = String::new();
+            s.push_str("<body>");
+            for id in &path {
+                let title = ENTRIES.get(id).unwrap().title;
+                s.push_str(
+                    &format!("<p><a href=\"https://{}.wikipedia.org/?curid={}\">`{}`</a></p>",
+                             lang, id, title));
+            }
+            s.push_str("</body>");
+            s
+        },
+        Err(Error::Terminated(t)) => format!("The search was terminated after {} rounds", t),
+        Err(Error::NoSuchPath) => String::from("No such path of any length exists"),
     }
 }
--- bfs/src/main.rs
+++ bfs/src/main.rs
@@ -1,11 +1,102 @@
 extern crate wikidata;
 extern crate bfs;
-//use wikidata::ENTRIES;
+extern crate clap;
+use std::io::{self, Write};
+use wikidata::ADDRESSES;
+use bfs::{bfs, search, print_path, load_titles};
+use clap::{App, Arg};
+
+// Notes about using:
+//  - Performing the same search multiple times might yield different paths
+//      (though always with the same length)
+//      An empty value for src/dst article titles means to use the prior value
+//      So after performing a search, hit Enter twice to do it again
+//  - Article searches are case sensitive
+//  - Search recommendations are based on substrings, not string similarity.
+//      So for a page to be recommended, your query must be a part of its article title
+//      Which is still case sensitive. (I know that's a little harsh)
+//  - Redirects are accounted for. "Ronald Reagan" and "Reagan" are treated as identical
+//  - Control-C to quit
+//
+//
 
 fn main() {
-    let a = wikidata::ADDRESSES.get("Rust").unwrap();
-    let b = wikidata::ADDRESSES.get("Alan_Turing").unwrap();
+    let matches = App::new("simple.wikilinks")
+        .version("1.0")
+        .author("owen <stensonowen@gmail.com>")
+        .about("Find link paths between simple.wikipedia.org articles (data from Dec 2016)")
+        .arg(Arg::with_name("links")
+            .short("l")
+            .long("links")
+            .help("Print followable links in paths"))
+        .get_matches();
+    load_titles();  //get this out of the way (probably doesn't have to be lazy)
+    let print_links = match matches.is_present("links") {
+        true  => Some("simple"),
+        false => None,
+    };
+    println!("{:?}", print_links);
 
-    let r = bfs::bfs(*a,*b);
-    bfs::print_path(r);
+    let mut src_id = None;
+    let mut dst_id = None;
+    loop {
+        println!("Search from one Simple Wikipedia article to another by links");
+        print!("Enter the title of the SOURCE article:       ");
+        src_id = Some(get_article(&src_id));
+        print!("Enter the title of the DESTINATION article:  ");
+        dst_id = Some(get_article(&dst_id));
+        print_path(bfs(src_id.unwrap(), dst_id.unwrap()), print_links);
+    }
+}
+
+fn get_article(prev: &Option<u32>) -> u32 {
+    //request an article from the user and return the page_id
+    //if it's invalid, try to give them some recommendations
+    //if they enter nothing, maybe default back to the previous value (`prev`)
+    let mut buffer = String::new();
+    let mut page_id: Option<u32> = None;
+    while page_id.is_none() {
+        buffer.clear();
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut buffer).expect("Failed to read input");
+        let fix = buffer.trim().replace(' ', "_");
+        if fix.is_empty() {
+            if let &Some(p) = prev {
+                return p;
+            } else {
+                println!("No prior article was found");
+                print!("Please enter another article title to try:   ");
+                continue;
+            }
+        }
+        page_id = match ADDRESSES.get(fix.as_str()) {
+            Some(id) => Some(*id),
+            None => {
+                let guesses = search(&fix);
+                if guesses.len() == 0 {
+                    println!("No such page or similar articles were found");
+                    print!("Please enter another article title to try:   ");
+                    continue;
+                }
+                println!("No page title found with that name. Perhaps you meant:");
+                for (i, g) in guesses.iter().enumerate() {
+                    println!("\t{}:\t`{}`", i, g);
+                }
+                print!("Enter the number corresponding to your selection, or 'x' to retry: ");
+                buffer.clear();
+                io::stdout().flush().unwrap();
+                io::stdin().read_line(&mut buffer).expect("Failed to read input");
+                match buffer.trim().parse::<usize>() {
+                    Ok(i @ 0...10) if i < guesses.len() => {
+                        Some(*ADDRESSES.get(guesses.get(i).unwrap()).unwrap())
+                    }
+                    _ => {
+                        print!("Please enter another article title to try:   ");
+                        None
+                    }
+                }
+            }
+        };
+    }
+    page_id.unwrap()
 }
