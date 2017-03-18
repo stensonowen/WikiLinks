d536858084e06f41eb85aa15beafb95d979d05eb
owen <owen@oms.sh>
2017-03-18 14:11:06 -0400

    Uhhhh lifetimes are hard and I didn't totally plan this out
    Might just need mutexes + clones for everything?
    Maybe that cost will be insignificant? idk.

--- links/src/cache/stack_cache.rs
+++ links/src/cache/stack_cache.rs
@@ -1,5 +1,5 @@
 
-use std::collections::{HashSet, LinkedList, BTreeSet, BinaryHeap, VecDeque};
+use std::collections::{HashSet, HashMap, LinkedList, BTreeSet, BinaryHeap, VecDeque};
 use std::cmp::{Ord, PartialOrd, Ordering};
 use std::sync::{Mutex, RwLock};
 
@@ -11,6 +11,42 @@ pub struct StackCache<'a> {
     // Very common read op: check smallest length
     // Quite common: random insert, pop last element
     length: RwLock<LengthCache<'a>>,
+}
+
+impl<'a> StackCache<'a> {
+    //fn insert_and_get_recent(&'a mut self, elem: CacheElem<'a>) -> &'a [CacheElem<'a>] {
+    //fn insert(&'a mut self, elem: CacheElem<'a>) {
+    fn insert(&'a mut self, elem: CacheElem<'a>) {
+        // always (try to) update recent list
+        if let Ok(r) = self.recent.get_mut() {
+            r.insert_elem(elem);
+        }
+        // if pertinent, update long list
+    }
+    /*
+    fn update_and_get_mut(&'a mut self, elem: CacheElem<'a>) -> Vec<CacheElem<'a>> {
+        if let Ok(r) = self.recent.get_mut() {
+            r.insert_elem(elem)
+        }
+        if let Ok(r) = self.recent.lock() {
+            //r.get().clone().to_vec()
+            vec![]
+        } else {
+            vec![]
+        }
+    }*/
+    //fn insert_and_get_recent(&'a mut self, elem: CacheElem<'a>) -> Vec<CacheElem<'a>> {
+    //fn get_recent(&'a mut self) -> Vec<CacheElem<'a>> {
+    fn get_recent(&'a self) -> Vec<CacheElem<'a>> {
+        let r = self.recent.lock().unwrap();
+        //r.get().to_vec()
+        unimplemented!()
+    }
+    //fn get_longest(&'a self) -> &'a [&'a CacheElem<'a>] {
+    fn get_longest(&'a self) -> Vec<&'a CacheElem<'a>> {
+        let l = self.length.read().unwrap();
+        l.get().to_vec()
+    }
 }
 
 struct RecentCache<'a> {
@@ -31,8 +67,10 @@ struct RecentCache<'a> {
 
     queue: VecDeque<CacheElem<'a>>,
     // rely on VecDeque.contains() every iteration? Or use a redundant hashset?
-    set:   HashSet<&'a CacheElem<'a>>,
-
+    //set:   HashSet<&'a CacheElem<'a>>,
+    contents:   HashSet<&'a CacheElem<'a>>,
+    //contents: HashMap<(u32,u32), &'a CacheElem<'a>>,
+    //contents: HashMap<CacheElem<'a>, &'a CacheElem<'a>>,
 }
 
 struct LengthCache<'a> {
@@ -47,6 +85,74 @@ struct LengthCache<'a> {
     // handle efficient lookups/inserts/etc.
     heap: BinaryHeap<CacheElem<'a>>,
     temp: Option<Vec<&'a CacheElem<'a>>>,
+}
+
+impl<'a> LengthCache<'a> {
+    fn min_len(&self) -> Option<u8> {
+        self.heap.peek().map(|e| e.len)
+    }
+    fn should_insert(&self, new: &CacheElem) -> bool {
+        if self.heap.len() < CACHE_SIZE {
+            true
+        } else if let Some(shortest) = self.heap.peek() {
+            new.len > shortest.len 
+        } else {
+            true 
+        }
+    }
+    pub fn get(&'a mut self) -> &[&CacheElem] {
+        match self.temp {
+            Some(ref v) => v,
+            None => self.rebuild_cache(),
+        }
+    }
+    fn rebuild_cache(&'a mut self) -> &[&CacheElem] {
+        let iter = self.heap.iter();
+        self.temp = Some(iter.collect());
+        match self.temp {
+            Some(ref v) => v,
+            None => unreachable!(),
+        }
+    }
+    fn insert_elem(&mut self, elem: CacheElem<'a>) {
+        // ONLY call iff should_insert
+        self.temp = None;
+        self.heap.push(elem);
+        if self.heap.len() > CACHE_SIZE {
+            self.heap.pop();
+        }
+    }
+}
+
+impl<'a> RecentCache<'a> {
+    //fn get(&self) -> &[CacheElem<'a>] {   // elides to what?
+    //pub fn get(&'a self) -> &'a [CacheElem<'a>] {
+    pub fn get(&self) -> &[CacheElem] {
+        // we never `push_front()`, so the contents will always be in the first array
+        // I think
+        let slices = self.queue.as_slices();
+        assert!(slices.1.is_empty());
+        slices.0
+    }
+    //pub fn insert_elem(&'a mut self, elem: CacheElem<'a>) {
+    pub fn insert_elem(&'a mut self, elem: CacheElem<'a>) {
+        // TODO: if elem is already present, move it to the from of the queue??
+        // for now, never remove elements except from the back
+        if self.contents.contains(&elem) {
+            // if new elem is already somewhere, that's fine
+            // this makes sense, right?
+            return;
+        }
+        if self.queue.len() >= CACHE_SIZE {
+            // if adding an element will make the queue too long, remove the least recent
+            let rem = self.queue.pop_front().unwrap();
+            self.contents.remove(&rem);
+        }
+        let index = self.queue.len();
+        self.queue.push_back(elem);
+        self.contents.insert(self.queue.get(index).unwrap());
+        //assert_eq!(Some(&elem), self.queue.get(index));
+    }
 }
 
 /*
@@ -92,7 +198,7 @@ impl<'a> StackCache<'a> {
 }
 */
 
-#[derive(Debug, Serialize, PartialEq, Eq, Hash)]
+#[derive(Debug, Serialize, PartialEq, Eq, Hash, Clone)]
 pub struct CacheElem<'a> {
     // TODO: will LLVM optimize equality comparison to compare numbers first?
     // or should we implement PartialEq ourselves?
